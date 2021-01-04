b96f87613c3e36bc056ee84724872d6fa5056ab5
Leo <marzolileo@gmail.com>
2021-01-04 17:57:12 +0100

    Aggiunto nuova struttura Questions Json

--- Assets/Leo/Scripts/QuestionManager.cs
+++ Assets/Leo/Scripts/QuestionManager.cs
@@ -10,8 +10,7 @@ namespace Leo.Scripts
         // Start is called before the first frame update
         void Start()
         {
-            Question question = new Question("Che tipo di linguaggio e' C#");
-            question.answers = new List<Answer>()
+            List<Answer> answers = new List<Answer>()
             {
                 new Answer("Procedurale",false),
                 new Answer("Ad Oggetti",true),
@@ -19,30 +18,47 @@ namespace Leo.Scripts
                 new Answer("Inglese",false)
             };
 
+            List<Question> questions = new List<Question>()
+            {
+                new Question("Che tipo di linguaggio e' C#", answers)
+            };
+            
+            QuestionLevel questionLevel = new QuestionLevel(questions);
+            
             //Creiamo il file Json della classe question
-            string json = JsonUtility.ToJson(question);
+            string json = JsonUtility.ToJson(questionLevel);
             Debug.Log(json);
             
             //andiamo a salvare il file Json
             File.WriteAllText(Application.dataPath + "/saveFile.json", json);
             
             //Estraiamo i dati dal Json e li mettiamo in deserializedQuestion
-            Question deserializedQuestion = JsonUtility.FromJson<Question>(json);
-            Debug.Log(deserializedQuestion.question);
-            Debug.Log(deserializedQuestion.answers.Count);
-            
+            QuestionLevel deserializedQuestionLevel = JsonUtility.FromJson<QuestionLevel>(json);
+            Debug.Log(deserializedQuestionLevel.questions);
+
         }
     }
+    [Serializable]
+    public class QuestionLevel
+    {
+        public List<Question> questions;
+        
+        public QuestionLevel(List<Question> questions)
+        {
+            this.questions = questions;
+        }
+    }
+    
     [Serializable]
     public class Question
     {
         public string question;
         public List<Answer> answers;
 
-        public Question(string question)
+        public Question(string question,List<Answer> answers)
         {
             this.question = question;
-            this.answers = new List<Answer>();
+            this.answers = answers;
         }
     }
     [Serializable]
