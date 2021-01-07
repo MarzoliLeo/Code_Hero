e872a85959fb1706f809622922b3b9e2f59e8642
Leo <marzolileo@gmail.com>
2021-01-07 19:12:20 +0100

    Il Timer inizia a muoversi quando il livello viene selezionato

--- Assets/Leo/Scripts/LevelManager.cs
+++ Assets/Leo/Scripts/LevelManager.cs
@@ -6,12 +6,16 @@ namespace Leo.Scripts
 {
     public class LevelManager : MonoBehaviour
     {
+        //Evento per far partire il timer quando il bottone di Start viene premuto.
+        public delegate void OnStartClick();
+        public static event  OnStartClick onStartClick;
 
         //Metodo per caricare un nuovo livello nel caso in cui si prema lo Start Button.
         //Il livello scelto sarà rispettivamente l'index del waypoint ad esso assocciato.
         public void LoadIndexLevel()
         {
             SceneManager.LoadScene(GameManager.Instance.DestinationWaypoint.levelIndex);
+            onStartClick?.Invoke();
         }
     }
 }
---
+++ Assets/Leo/Scripts/TimerCountdown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Leo.Scripts;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class TimerCountdown : MonoBehaviour
+{
+    public Slider timeToAnswer;
+    public GameObject 
+
+    //Collegamento all'evento;
+    private void OnEnable()
+    {
+        LevelManager.onStartClick += Update;
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.onStartClick -= Update;
+    }
+    private void Update()
+    {
+        timeToAnswer.value -= Time.deltaTime;
+        if (timeToAnswer.value < 5)
+        {
+            timeToAnswer. = Mathf.Lerp(87, 45, Time.deltaTime);
+        }
+        if (timeToAnswer.value < Mathf.Epsilon)
+        {
+            Debug.Log("Timer finito hai perso");
+        }
+    }
+}
