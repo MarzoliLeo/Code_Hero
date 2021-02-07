cae668f24784af960b9e95d77da7f5e2c820a1e1
Leo <marzolileo@gmail.com>
2021-02-07 12:18:40 +0100

    Aggiunto I livelli 3,4 con rispettivi Json e implementazioni

--- Assets/Leo/Scripts/GameManager.cs
+++ Assets/Leo/Scripts/GameManager.cs
@@ -108,7 +108,7 @@ namespace Leo.Scripts
         private void OnSceneLoaded(Scene scene,LoadSceneMode mode)
         {
             //Controlla che non venga fatta nessuna inizializzazione nel menù
-            if (scene.buildIndex != 0 || scene.buildIndex != 4)
+            if (scene.buildIndex != 0 /*|| scene.buildIndex != TransitionScene*/)
             {
                 
                 _playerRef = FindObjectOfType<Player>();
