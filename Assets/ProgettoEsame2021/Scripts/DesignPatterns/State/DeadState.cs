5c6b5c5c5dd4b0eeb8e0bc036a954e12e6517687
Leo <marzolileo@gmail.com>
2021-04-01 20:57:20 +0200

    Aggiornato lo State Pattern e finito tutto.
    Non c'è più nulla da fare.

--- Assets/ProgettoEsame2021/Scripts/DesignPatterns/State/DeadState.cs
+++ Assets/ProgettoEsame2021/Scripts/DesignPatterns/State/DeadState.cs
@@ -9,6 +9,8 @@ namespace ProgettoEsame2021.Scripts.DesignPatterns.State
         public ICharacterState DoState(CharacterBase character)
         {
             Debug.Log("Sono nello stato di morte!");
+            //cambia il colore del character.
+            character.GetComponent<SpriteRenderer>().color = Color.red;
             return character.idleState;
         }
     }
